35580bb314e5c5b5cb28511631c949674b4c6db2
toadless <thomasmcdonald2027@gmail.com>
2022-01-28 19:22:30 +0000

    change docker stuff

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:15-jdk
-WORKDIR /home/monke/
+WORKDIR /home/monkebot/
 COPY build/libs/Monke-all.jar Monke.jar
 ENTRYPOINT java -jar Monke.jar
--- createTagBackup.sh
+++ createTagBackup.sh
@@ -2,7 +2,7 @@ git pull
 ./gradlew clean shadowJar
 docker build .
 tagName=$(docker images | awk '{print $3}' | awk 'NR==2')
-docker tag $tagName toadlesss/monke:latest
+docker tag $tagName toadlesss/monkebot:latest
 docker login
-docker push toadlesss/monke:latest
+docker push toadlesss/monkebot:latest
 echo "Done!"
