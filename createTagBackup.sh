397fdb46b690c8fd4fb3e732ec3246ef558e686c
toad <thomasmcdonald2027@gmail.com>
2022-03-05 09:49:21 +0000

    Update createTagBackup.sh

--- createTagBackup.sh
+++ createTagBackup.sh
@@ -2,7 +2,7 @@ git pull
 ./gradlew clean shadowJar
 docker build .
 tagName=$(docker images | awk '{print $3}' | awk 'NR==2')
-docker tag $tagName toadlesss/monkebot:latest
+docker tag $tagName frogg1t/monkebot:latest
 docker login
-docker push toadlesss/monkebot:latest
+docker push frogg1t/monkebot:latest
 echo "Done!"
